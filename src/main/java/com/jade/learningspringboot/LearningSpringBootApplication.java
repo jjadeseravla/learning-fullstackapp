9fab68fb06c137d5543a33f6df3e42681c9994ac
jjadeseravla <jadealvares@hotmail.co.uk>
2020-04-22 10:55:25 +0100

    created a spring boot restful API

--- src/main/java/com/jade/learningspringboot/LearningSpringBootApplication.java
+++ src/main/java/com/jade/learningspringboot/LearningSpringBootApplication.java
@@ -2,12 +2,48 @@ package com.jade.learningspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringBootApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LearningSpringBootApplication.class, args);
+	}
+
+	@RestController
+	class Resource {
+
+		@GetMapping
+		Message getMessage() {
+			return new Message("Hello Jade");
+		}
+
+	}
+
+	//this message class is a model so dont need to inject as we want to create a bunch of these
+	class Message {
+
+		private final String message;
+
+
+		public Message(String message) {
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		@Override
+		public String toString() {
+			return "Message{" +
+					"message='" + message + '\'' +
+					'}';
+		}
+
 	}
 
 }
