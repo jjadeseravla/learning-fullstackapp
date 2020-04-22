0a608080cc66fc3e33021150cea14d31088b4e85
jjadeseravla <jadealvares@hotmail.co.uk>
2020-04-22 11:09:04 +0100

    DAO interface (DAO interacts with DB)

---
+++ src/main/java/com/jade/learningspringboot/dao/UserDao.java
@@ -0,0 +1,20 @@
+package com.jade.learningspringboot.dao;
+
+import com.jade.learningspringboot.model.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserDao {
+
+    List<User> getAllUsers();
+
+    User getUser(UUID userId);
+
+    int updateUser(User user);
+
+    int removeUser(UUID userId);
+
+    int insertUser(User user);
+
+}
