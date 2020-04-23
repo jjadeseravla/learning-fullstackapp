fa0a4beabde7ca1b4338a98032ce1ca17e2ce215
jjadeseravla <jadealvares@hotmail.co.uk>
2020-04-23 09:39:23 +0100

    Bad way of using the userDao in service, next commit will be with dependency injection

--- src/main/java/com/jade/learningspringboot/service/UserService.java
+++ src/main/java/com/jade/learningspringboot/service/UserService.java
@@ -1,19 +1,26 @@
 package com.jade.learningspringboot.service;
 
+import com.jade.learningspringboot.dao.FakeDataDao;
+import com.jade.learningspringboot.dao.UserDao;
 import com.jade.learningspringboot.model.User;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class UserService {
 
-    public List<User> getAllUsers() {
+    private UserDao userDao;
 
+    public UserService() {
+        this.userDao = new FakeDataDao();
+    }
+
+    public List<User> getAllUsers() {
+        return null;
     }
 
     public User getUser(UUID userId) {
-
+        return null;
     }
 
     public int updateUser(User user) {
