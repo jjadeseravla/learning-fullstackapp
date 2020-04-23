6d5b9942abbfea3ec84e49c2230b0c2dfbb7a2f0
jjadeseravla <jadealvares@hotmail.co.uk>
2020-04-23 10:34:48 +0100

    select user by user id test and ensure not selected user with random id

--- src/test/java/com/jade/learningspringboot/dao/FakeDataDaoTest.java
+++ src/test/java/com/jade/learningspringboot/dao/FakeDataDaoTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,8 +36,31 @@ class FakeDataDaoTest {
     }
 
     @Test
-    void selectUserByUserId() {
+    void shouldSelectUserByUserId() {
+        UUID userLiamId = UUID.randomUUID();
+        User liam = new User(userLiamId,
+                "Liam",
+                User.Gender.MALE,
+                37);
+        fakeDataDao.insertUser(userLiamId, liam);
+
+        assertThat(fakeDataDao.selectAllUsers()).hasSize(2);
+        //assertThat(fakeDataDao.selectUserByUserId(userLiamId).isPresent());
+
+        //liamOptional is from DB and liam is the User liam of this test
+        Optional<User> liamOptional = fakeDataDao.selectUserByUserId(userLiamId);
+        assertThat(liamOptional.isPresent()).isTrue();
+        assertThat(liamOptional.get()).isEqualToComparingFieldByField(liam);
     }
+
+    @Test
+    void shouldNotSelectUserByRandomUserId() {
+        Optional<User> optionalUserOfRandomId = fakeDataDao.selectUserByUserId(UUID.randomUUID());
+
+        assertThat(optionalUserOfRandomId.isPresent()).isFalse();
+     }
+
+
 
     @Test
     void updateUser() {
