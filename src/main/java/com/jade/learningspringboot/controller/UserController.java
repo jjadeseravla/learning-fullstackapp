c052b450720f45e77e3f8989682e33b6897091da
jjadeseravla <jadealvares@hotmail.co.uk>
2020-04-24 11:25:09 +0100

    Ass a path

--- src/main/java/com/jade/learningspringboot/controller/UserController.java
+++ src/main/java/com/jade/learningspringboot/controller/UserController.java
@@ -4,11 +4,13 @@ import com.jade.learningspringboot.model.User;
 import com.jade.learningspringboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/api/v1/users")
 public class UserController {
 
     public UserService userService; //dependency injection
