c9e8ae66ca26f6e37aee383adf670f78af44d2b0
jjadeseravla <jadealvares@hotmail.co.uk>
2020-04-24 11:28:23 +0100

    GET single user
    use @PathVariable to pass a variable from the URL to the method

--- src/main/java/com/jade/learningspringboot/controller/UserController.java
+++ src/main/java/com/jade/learningspringboot/controller/UserController.java
@@ -4,15 +4,18 @@ import com.jade.learningspringboot.model.User;
 import com.jade.learningspringboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api/v1/users")
 public class UserController {
 
+    private static final java.util.UUID UUID = ;
     public UserService userService; //dependency injection
 
     @Autowired
@@ -23,5 +26,10 @@ public class UserController {
     @GetMapping
     public List<User> fetchUsers() {
         return userService.getAllUsers();
+    }
+
+    @GetMapping(path = "{}") //a value that we will pass in the path
+    public User fetchUser(@PathVariable("userId") UUID userId) {
+        return userService.getUser(userId).get(); //.get() because getUser() is an Optional
     }
 }
