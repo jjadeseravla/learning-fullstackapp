660e6e999c44a60a8f5a55eb99f5929cfe1a7c31
jjadeseravla <jadealvares@hotmail.co.uk>
2020-04-24 11:53:48 +0100

    if user present we pass a 200 status code and then pass along a user which is our payload.  Otherwise return a 404 with a message in the boyd

--- src/main/java/com/jade/learningspringboot/controller/UserController.java
+++ src/main/java/com/jade/learningspringboot/controller/UserController.java
@@ -3,12 +3,15 @@ package com.jade.learningspringboot.controller;
 import com.jade.learningspringboot.model.User;
 import com.jade.learningspringboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -29,7 +32,14 @@ public class UserController {
     }
 
     @GetMapping(path = "{}") //a value that we will pass in the path
-    public User fetchUser(@PathVariable("userId") UUID userId) {
-        return userService.getUser(userId).get(); //.get() because getUser() is an Optional
+    public ResponseEntity<?> fetchUser(@PathVariable("userId") UUID userId) {
+    //? cos i dont know if we are going to find this user or not
+        Optional<User> optionalUser = userService.getUser(userId);
+        if (optionalUser.isPresent()) {
+            return ResponseEntity.ok(optionalUser.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("user" + userId + "no found");
     }
+
+
 }
